15a0b28a0572c025cba57067234e03cc48f19b76
Steven <steven.a.pinkerton@gmail.com>
2022-05-16 19:47:16 +0200

    new revision

--- flake.nix
+++ flake.nix
@@ -1,63 +1,8 @@
-{ config, pkgs, ...}:
-
-{
-nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = ''
-        experimental-geatures = nix-command flakes
-        '';
-    };
-
-    system = {
-        stateVersion = "21.11";
-        };
-
-    time.timeZone = "Europe/Amsterdam";
-
-    services = {
-        tailscape.enable = true;
-        nginx.enable = true;
-        openssh = {
-          enable = true;
-          passwordAuthentication = false;
-          permitRootLogin = "no";
-          challengeResponseAuthentication = false;
-        };
-    };
-
-    environment = {
-        variables = {
-            EDITOR ="nvim";
-            LC_ALL = config.i18n.defaultLocale;
-            TERM= "xterm-s56color";
-        };
-    };
-
-    nixpkgs.config.allowUnfree = true;
-
-        boot.loader.grub.enable = true;
-        boot.loaded.grub.version = 2;
-        boot.loader.grub.device = "/dev/sda";
-
-        networking.useDHCP = false;
-        networking.interfaces.ens33.useDHCP = true;
-
-    i18.defaultLocale = "en_GB.UTF-8";
-        console = {
-            font = "Lat2-Terminus16";
-            keyMap = "uk";
-        };
-
-    users.users.steven = {
-        isNormalUser = true;
-        initalHashedPassword = "$6$2IMeBUr3ehYkkF9p$popywjgNmIsi1pSdE1AtHH29mHjUVPAgJwsxRoAoMt0bEoovw.A5P7Y2wo0xO611JQizf0DCMV9UWIXpGdyxt/";
-        extraGroups = [ "wheel" ];
-    };
-
-    environment.systemPackages = with pkgs; [
-        neovim
-    ];
-    programs.bash.shellInit = ''
-    TERM=xterm
-    '';
+outputs = { self, nixpkgs ...}: {
+  nixosConfigurations = {
+    "mySystem" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
     }
+  }
+}
