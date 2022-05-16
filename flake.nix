a370df7a9b70e37c0509416ab7b5912bda6edb50
Steven <steven.a.pinkerton@gmail.com>
2022-05-16 19:52:31 +0200

    next revision

--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
-outputs = { self, nixpkgs ...}: {
+{ 
+  outputs = { self, nixpkgs, ...}: {
   nixosConfigurations = {
     "mySystem" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./configuration.nix ];
-    }
-  }
+    };
+  };
+};
 }
