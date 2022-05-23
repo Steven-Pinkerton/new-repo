e1e20b7c24459298ae8edba2846d0031c029640b
Steven <steven.a.pinkerton@gmail.com>
2022-05-23 15:30:12 +0200

    next revision

--- flake.nix
+++ flake.nix
@@ -3,15 +3,24 @@
 
   inputs = {
       nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+      nixinate.url = "github:matthewcroughan/nixinate";
      };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nixinate }:
   {
+      apps = nixinate.nixinate.x86_64-linux self;
       nixosConfigurations.remote-machine = nixpkgs.lib.nixosSystem {
          system = "x86_64-linux";
          
          modules = [ 
            (import ./configuration.nix) 
+           {
+             _module.args.nixinate =  {
+               host = "193.16.42.17";
+               sshUser = "Steven";
+               buildOn = "remote";
+             }
+           }
            ];
          };
   };
