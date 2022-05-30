d78eee6891ffa0946ab14798475660dc03aa1fa0
Steven <steven.a.pinkerton@gmail.com>
2022-05-30 16:47:11 +0200

    changed some extraconfig stuff

--- configuration.nix
+++ configuration.nix
@@ -67,11 +67,12 @@
                 proxyPass = "http://vouch.dirunum.platonic.systems:9090";
                 extraConfig = ''
                     proxy_set_header Host $host;
-                    proxy_set_request_body off;
+                    proxy_pass_request_body off;
+                    proxy_set_header Content-Length "";
                     auth_request_set $auth_resp_x_vouch_user $upstream_http_x_vouch_user;
-                    #auth_request_set $auth_resp_jwt $upstream_http_x_vouch_jwt;
-                    #auth_request_set $auth_resp_err $upstream_http_x_vouch_err;
-                    #auth_request_set $auth_resp_failcount$upstream_http_x_vouch_failcount;
+                    auth_request_set $auth_resp_jwt $upstream_http_x_vouch_jwt;
+                    auth_request_set $auth_resp_err $upstream_http_x_vouch_err;
+                    auth_request_set $auth_resp_failcount$upstream_http_x_vouch_failcount;
             '';
             };
             
