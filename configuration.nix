58816264914626c833c06807efbe8829b2304254
Steven-Pinkerton <steven.a.pinkerton@gmail.com>
2022-06-07 21:59:18 +0100

    whitesapce

--- configuration.nix
+++ configuration.nix
@@ -41,7 +41,7 @@
               after = [ "network.target" ];
               wantedBy = [ "multi-user.target" ];
               serviceConfig = {
-                        ExecStart = pkgs.writeShellScript "vouch-proxy.sh" 
+                      ExecStart = pkgs.writeShellScript "vouch-proxy.sh" 
                         ''
                       set -x
                       if ! [[ -d /var/lib/vouch-proxy ]]; then
