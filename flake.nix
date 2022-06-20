646fd96d84d464065db5e083f5af7b4b531eaa38
Steven-Pinkerton <steven.a.pinkerton@gmail.com>
2022-06-20 16:53:22 +0100

    lowercase

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
               host = "128.199.11.198";
               sshUser = "root";
               buildOn = "remote";
-              Hermetic = true;
+              hermetic = true;
             };
           }
         ];
